b9475a0febda96169699b02468ccfad93951329a
Biswajit Paria <biswajitsc@gmail.com>
2016-04-29 17:56:52 +0530

    fixed norm error

--- AML_TP/svm_multiclass/find_norm_error.m
+++ AML_TP/svm_multiclass/find_norm_error.m
@@ -1,15 +1,16 @@
 function error = find_norm_error(A,A_proj)
 
-dim = size(A);
-num = dim(1);
-V = A*A';
-V_ = A_proj*A_proj';
+    dim = size(A);
+    n = dim(1); m = dim(2);
+    
+    V = A*A';
+    V_ = A_proj*A_proj';
 
     diff = (V - V_);
     diff = diff.*diff;
     diff = sum(diff);
     diff = sum(diff);
-    diff = diff/num/num;
+    diff = diff/n/m;
     diff = sqrt(diff);
     error = diff;
 end
