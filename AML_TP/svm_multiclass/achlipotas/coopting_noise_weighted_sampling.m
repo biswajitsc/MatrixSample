1aa5da7bcd6947204721f8481b12cf66da833d45
Biswajit Paria <biswajitsc@gmail.com>
2016-04-29 16:02:05 +0530

    reverting to 642b4676edc6ed17a6d83aadb295a11c32ee7232

--- AML_TP/svm_multiclass/achlipotas/coopting_noise_weighted_sampling.m
+++ AML_TP/svm_multiclass/achlipotas/coopting_noise_weighted_sampling.m
@@ -3,10 +3,8 @@ function A_proj =coopting_noise_weighted_sampling(A)
 %Assumes A(i,j) to be within -1 to +1
 
 dim = size(A);
-A_proj = zeros(dim(1),dim(2));
+A_proj = zeros(dim[1],dim[2]);
 p = rand(1,1);
-d = dim(1); k = dim(2);
-
 for i = [1:d]
     for j = [1:k]
         if p > 0 & A(i,j) > 0 & rand(1,1) < p*square(A(i,j))
--- AML_TP/svm_multiclass/find_norm_error.m
+++ AML_TP/svm_multiclass/find_norm_error.m
@@ -1,5 +1,5 @@
 function error = find_norm_error(A,A_proj)
-error = 0.0;
+
 dim = size(A);
 num = dim(1);
 V = A*A';
@@ -9,11 +9,10 @@ V_ = A_proj*A_proj';
     diff = diff.*diff;
     diff = sum(diff);
     diff = sum(diff);
-%     diff = diff/num/num;
-%     diff = sqrt(diff);
-%     orig_norm = norm(A(i,:));
-%     proj_norm = norm(A_proj(i,:));
-%     error = error + abs(orig_norm - proj_norm)/orig_norm;
-error = diff
+    diff = diff/num/num;
+    diff = sqrt(diff);
+    error = diff;
+    
+end
 
 % error = error/num;
